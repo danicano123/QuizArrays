8977935478aad9139e4ee51a15ac7a8d2abfe94a
Daniel Steven Cano Zapata <daniprog2000@gmail.com>
2023-03-17 19:56:59 -0500

    FEAT: final delivery

---
+++ QuizArray/OneDimension/OneDimension.cs
@@ -0,0 +1,63 @@
+﻿namespace QuizArrays.Logic
+{
+    public class OneDimension
+    {
+        private int[] _array;
+        private int _top = 0;
+
+        public OneDimension(int n)
+        {
+            _array = new int[n];
+        }
+
+        private bool IsEmpty
+        {
+            get
+            {
+                if (_top == 0)
+                {
+                    return true;
+                }
+                return false;
+
+            }
+        }
+
+        private bool IsFull
+        {
+            get
+            {
+                if (_top == N)
+                {
+                    return true;
+                }
+                return false;
+
+            }
+        }
+
+        public int N
+        {
+            get => _array.Length;
+        }
+
+        public void Add(int n)
+        {
+            if (IsFull) { throw new Exception("Array is full!"); } else _array[_top] = n;
+
+            _top++;
+        }
+
+        public override string ToString()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                output += $"{_array[i]}\t";
+            }
+            return output;
+        }
+
+
+    }
+}
---
+++ QuizArray/OneDimension/TwoDimensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizArrays.Logic
+{
+    public class TwoDimensions
+    {
+        int[,] _array;
+        public TwoDimensions(int m, int n)
+        {
+            _array = new int[m, n];
+        }
+
+        public int M
+        {
+            get => _array.GetLength(0);
+        }
+        public int N
+        {
+            get => _array.GetLength(1);
+        }
+
+        public void Fill()
+        {
+            var random = new Random();
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    _array[i, j] = random.Next(1, 100);
+                }
+            }
+        }
+
+        public string ToOneDimension()
+        {
+
+            int sizeNewArray = N * M;
+            var one = new OneDimension(sizeNewArray);
+            int counter = 0;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    one.Add(_array[i, j]);
+                    counter++;
+                }
+            }
+            return one.ToString();
+        }
+
+        public override string ToString()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    output += $"{_array[i, j]}\t";
+                }
+                output += "\n";
+            }
+            return output;
+        }
+
+    }
+}
